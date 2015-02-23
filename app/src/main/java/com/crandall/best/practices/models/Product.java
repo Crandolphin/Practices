6db03b5dc7acada128e1652cccf03d9045441587
Crandall Stephenson <crandall.stephenson@gmail.com>
2015-02-23 04:22:57 -0800

    Renamed and set member fields in Product to private

--- app/src/main/java/com/crandall/best/practices/models/Product.java
+++ app/src/main/java/com/crandall/best/practices/models/Product.java
@@ -1,43 +1,49 @@
 package com.crandall.best.practices.models;
 
 
-/**
- * Created by Crandall on 2/20/2015.
- */
 public class Product {
 
-    String imageUrl;
-    String productName;
-    String productDescription;
+    /**
+     * The URL for the image of the product.
+     */
+    private String mImageUrl;
+    /**
+     * The name of the product.
+     */
+    private String mProductName;
+    /**
+     * The description of the product.
+     */
+    private String mProductDescription;
 
-    public Product(String imageUrl, String productName, String productDescription){
-        this.imageUrl = imageUrl;
-        this.productName = productName;
-        this.productDescription = productDescription;
+    public Product(String imageUrl, String productName, String productDescription) {
+        this.mImageUrl = imageUrl;
+        this.mProductName = productName;
+        this.mProductDescription = productDescription;
 
     }
 
     public String getImageUrl() {
-        return imageUrl;
+        return mImageUrl;
     }
 
     public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
+        this.mImageUrl = imageUrl;
     }
 
     public String getProductName() {
-        return productName;
+        return mProductName;
     }
 
     public void setProductName(String productName) {
-        this.productName = productName;
+        this.mProductName = productName;
     }
 
     public String getProductDescription() {
-        return productDescription;
+        return mProductDescription;
     }
 
     public void setProductDescription(String productDescription) {
-        this.productDescription = productDescription;
+        this.mProductDescription = productDescription;
     }
 }
